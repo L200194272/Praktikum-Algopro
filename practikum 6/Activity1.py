68642a9e19439d807fb6943ecab7946c003be490
L200194272 <57612010+L200194272@users.noreply.github.com>
2019-11-11 12:28:12 +0700

    Add files via upload

---
+++ practikum 6/Activity1.py
@@ -0,0 +1,22 @@
+Name = "Kayima Aisha"
+Address = "pesma mansur ums"
+Dateofbirth = "8 April 1999"
+Nationality ="Ugandan"
+countryoforigin ="uganda"
+Gender ="female"
+Age ="20"
+occupation ="student"
+countryofresidence ="Indonesia"
+Nimnumber ="L200194272"
+maritalstatus ="single"
+print(Name)
+print(Address)
+print(Dateofbirth)
+print(Nationality)
+print(countryoforigin)
+print(Gender)
+print(Age)
+print(occupation)
+print(countryofresidence)
+print(Nimnumber)
+print(maritalstatus)
---
+++ practikum 6/Activity2.py
@@ -0,0 +1,8 @@
+Name ="kayima Aisha"
+Dateofbirth ="8 April 1999"
+Username = Name[7] + Dateofbirth[0] + Dateofbirth[8:]
+Shortname = Name[7:]
+Password = Name[7:9] + "4" + Dateofbirth[0] + "6"
+print(Username)
+print(Shortname)
+print(Password)
